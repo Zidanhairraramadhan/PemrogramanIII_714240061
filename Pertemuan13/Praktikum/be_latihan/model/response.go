9258311d27071970a9e5b2e6e29c921c85f6dae2
Zidan Hairra Ramadhan <zidanramadhan950@gmail.com>
2026-06-20 23:31:03 +0700

    Menambahkan praktikum pertemuan 13

---
+++ Pertemuan13/Praktikum/be_latihan/model/response.go
@@ -0,0 +1,7 @@
+package model
+
+type Response struct {
+	Message string      `json:"message" example:"detail pesan"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty" example:"detail error"`
+}
